d4c81915cb251e4ceac7cd7c9cbd8cd5efc1d173
王月功 <yuegongwang@sohu-inc.com>
2019-05-01 20:05:36 +0800

    实现数据到视图的更新

--- test/vue.js
+++ test/vue.js
@@ -86,6 +86,30 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./lib/Dep.js":
+/*!********************!*\
+  !*** ./lib/Dep.js ***!
+  \********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n// 订阅发布中心，即消息管理员（Dep）\n// 它负责储存订阅者和消息的分发，不管是订阅者还是发布者都需要依赖于它\nclass Dep {\n    constructor() {\n        // 储存订阅者的数组\n        this.subs = [];\n    }\n\n    // 添加订阅者 subscribe\n    addSub(sub) {\n        this.subs.push(sub);\n    }\n\n    // 通知更新，注意：约定所有的订阅者实例都有一个 update 方法，用于同一进行更新\n    notify() {\n        this.subs.forEach(sub => sub.update());\n    }\n}\n\n// 静态属性，默认为 null，工作时指向当前的 Watcher\nDep.target = null;\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (Dep);\n\n//# sourceURL=webpack:///./lib/Dep.js?");
+
+/***/ }),
+
+/***/ "./lib/Watcher.js":
+/*!************************!*\
+  !*** ./lib/Watcher.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _Dep__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Dep */ \"./lib/Dep.js\");\n\n\n// 订阅者(Watcher)\nclass Watcher {\n    // vue 实例，compile 中正则匹配结果(即需要订阅的数据)，compile 中执行渲染更新的回调\n    constructor(vm, exp, cb) {\n        this.vm = vm;\n        this.exp = exp;\n        this.cb = cb;\n        this.val = this.get();\n    }\n\n    // 更新\n    update() {\n        const val = this.get();\n        if (val !== this.val) {\n            this.val = val;\n            this.cb.call(this.vm, val);\n        }\n    }\n\n    // 获取值的方法\n    get() {\n        _Dep__WEBPACK_IMPORTED_MODULE_0__[\"default\"].target = this;\n\n        let val = this.vm;\n        let arr = this.exp.split('.');\n        arr.forEach(key => val = val[key]);\n\n        _Dep__WEBPACK_IMPORTED_MODULE_0__[\"default\"].target = null;\n        return val;\n    }\n}\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (Watcher);\n\n//# sourceURL=webpack:///./lib/Watcher.js?");
+
+/***/ }),
+
 /***/ "./lib/compile.js":
 /*!************************!*\
   !*** ./lib/compile.js ***!
@@ -94,7 +118,7 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n// 2. 编译模板\nclass Compile {\n    constructor(el, vm) {\n        vm.$el = document.querySelector(el);\n        this.vm = vm;\n        this.render();\n    }\n\n    render() {\n        // 为了提高渲染效率，使用文本片段进行批量操作\n        let fragment = document.createDocumentFragment();\n        let child;\n        while(child = this.vm.$el.firstChild) {\n            fragment.appendChild(child);\n        }\n\n        // 处理过程：将 {{x}} 格式的占位替换成数据\n        this.replace(fragment);\n\n        // 处理完后再插回 DOM 中\n        this.vm.$el.appendChild(fragment);\n    }\n\n    replace(father) {\n        Array.from(father.childNodes).forEach(node => {\n            let text = node.textContent;\n            let reg = /\\{\\{(.*)\\}\\}/;\n\n            // 只处理文本节点类型\n            if(node.nodeType === 3 && reg.test(text)) {\n                // RegExp.$1 获取当前匹配的第一个子表达式，$1 静态属性非标准的，仅限学习时使用\n                let arr = RegExp.$1.split('.');\n                // 遍历一直向后取key\n                let val = this.vm;\n                arr.forEach(key => val = val[key]);\n                node.textContent = text.replace(reg, val);\n            }\n\n            // 递归处理子节点\n            if(node.childNodes) {\n                this.replace(node);\n            }\n        })\n    }\n}\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (Compile);\n\n//# sourceURL=webpack:///./lib/compile.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _Watcher__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Watcher */ \"./lib/Watcher.js\");\n\n\n// 2. 编译模板\nclass Compile {\n    constructor(el, vm) {\n        vm.$el = document.querySelector(el);\n        this.vm = vm;\n        this.render();\n    }\n\n    render() {\n        // 为了提高渲染效率，使用文本片段进行批量操作\n        let fragment = document.createDocumentFragment();\n        let child;\n        while(child = this.vm.$el.firstChild) {\n            fragment.appendChild(child);\n        }\n\n        // 处理过程：将 {{x}} 格式的占位替换成数据\n        this.replace(fragment);\n\n        // 处理完后再插回 DOM 中\n        this.vm.$el.appendChild(fragment);\n    }\n\n    replace(father) {\n        Array.from(father.childNodes).forEach(node => {\n            let text = node.textContent;\n            let reg = /\\{\\{(.*)\\}\\}/;\n\n            // 只处理文本节点类型\n            if(node.nodeType === 3 && reg.test(text)) {\n                // RegExp.$1 获取当前匹配的第一个子表达式，$1 静态属性非标准的，仅限学习时使用\n                let arr = RegExp.$1.split('.');\n                // 遍历一直向后取key\n                let val = this.vm;\n                arr.forEach(key => val = val[key]);\n                node.textContent = text.replace(reg, val);\n\n                // 创建订阅者实例，用于后续数据更新时的重新渲染\n                new _Watcher__WEBPACK_IMPORTED_MODULE_0__[\"default\"](this.vm, RegExp.$1, function(newVal) {\n                    node.textContent = text.replace(reg, newVal);\n                })\n            }\n\n            // 递归处理子节点\n            if(node.childNodes) {\n                this.replace(node);\n            }\n        })\n    }\n}\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (Compile);\n\n//# sourceURL=webpack:///./lib/compile.js?");
 
 /***/ }),
 
@@ -106,7 +130,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n// 2. 编译模板\nclass Com
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n// 监听者(Observer)，用于监听属性值的变化，也可以叫发布者\n\n// 1. 监听数据\nfunction observe(value) {\n    // 当值不存在，或者不是复杂数据类型时，不再需要继续深入监听（简单能力判断，防报错）\n    if (!value || typeof value !== 'object') {\n        return;\n    }\n    return new Observer(value);\n}\n// 监听者，监听对象属性值的变化\nclass Observer {\n    constructor(value) {\n        this.value = value;\n        this.walk(value);\n    }\n    // 遍历所有可枚举属性值并监听\n    walk(value) {\n        Object.keys(value)\n            .forEach(key => this.convert(key, value[key]));\n    }\n    // 执行监听的具体方法\n    convert(key, val) {\n        defineReactive(this.value, key, val);\n    }\n}\n\n// 定义响应\nfunction defineReactive(obj, key, val) {\n    // 1.4 深度遍历添加监听\n    observe(val);\n    Object.defineProperty(obj, key, {\n        enumerable: true, // 可枚举\n        configurable: true, // 可配置\n        get: () => { // 1.1 定义 getter 读访问操作符\n            return val;\n        },\n        set: newVal => { // 1.1 定义 setter 写访问操作符\n            // 1.2 值不变则不需要任何操作\n            if (val === newVal) return;\n            // 1.3 值改变以后，getter 获取的值应该是新值\n            val = newVal;\n            // 1.5 对新值进行监听\n            observe(newVal);\n        },\n    });\n}\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (observe);\n\n//# sourceURL=webpack:///./lib/observe.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _Dep__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Dep */ \"./lib/Dep.js\");\n\n// 监听者(Observer)，用于监听属性值的变化，也可以叫发布者\n\n// 1. 监听数据\nfunction observe(value) {\n    // 当值不存在，或者不是复杂数据类型时，不再需要继续深入监听（简单能力判断，防报错）\n    if (!value || typeof value !== 'object') {\n        return;\n    }\n    return new Observer(value);\n}\n// 监听者，监听对象属性值的变化\nclass Observer {\n    constructor(value) {\n        this.value = value;\n        this.walk(value);\n    }\n    // 遍历所有可枚举属性值并监听\n    walk(value) {\n        Object.keys(value)\n            .forEach(key => this.convert(key, value[key]));\n    }\n    // 执行监听的具体方法\n    convert(key, val) {\n        defineReactive(this.value, key, val);\n    }\n}\n\n// 定义响应\nfunction defineReactive(obj, key, val) {\n    // 1.4 深度遍历添加监听\n    observe(val);\n    let dep = new _Dep__WEBPACK_IMPORTED_MODULE_0__[\"default\"]();\n    Object.defineProperty(obj, key, {\n        enumerable: true, // 可枚举\n        configurable: true, // 可配置\n        get: () => { // 1.1 定义 getter 读访问操作符\n            // 收集依赖\n            _Dep__WEBPACK_IMPORTED_MODULE_0__[\"default\"].target && dep.addSub(_Dep__WEBPACK_IMPORTED_MODULE_0__[\"default\"].target);\n            return val;\n        },\n        set: newVal => { // 1.1 定义 setter 写访问操作符\n            // 1.2 值不变则不需要任何操作\n            if (val === newVal) return;\n            // 1.3 值改变以后，getter 获取的值应该是新值\n            val = newVal;\n            // 1.5 对新值进行监听\n            observe(newVal);\n            // 通知更新\n            dep.notify();\n        },\n    });\n}\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (observe);\n\n//# sourceURL=webpack:///./lib/observe.js?");
 
 /***/ }),
 
